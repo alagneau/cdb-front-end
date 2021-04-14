368467b2bcddb70f718ed34a656e53dede8a4951
Cyril <cyril.perie96@gmail.com>
2021-04-14 14:01:47 +0200

    Init routing \n Faire npm install

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {/* La page à afficher par défaut */}
+        </Route>
+        <Route path="/page">
+          {/* Une page */}
+        </Route>
+        {/* A garder en dernier, permet de redirigier vers Home en cas de aucun lien trouvé */}
+        <Route path="/">
+          {/* La page à afficher par défaut */}
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
